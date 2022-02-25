bdddec57a21da08a506e16212c0058fb5db102ba
ryuseifujisaki <fujiryu1122@gmail.com>
2022-02-26 03:35:04 +0900

    add about.tsx

---
+++ hello-next/pages/about.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+export default function About() {
+  return (
+    <div>
+      <head>
+        <title>自己紹介タイトル</title>
+        <meta name = "description" content='自己紹介概要'/>
+      </head>
+      <h1>藤崎 竜成</h1>
+    </div>
+  )
+}
